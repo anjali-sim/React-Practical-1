37c4c3b127b4b6f0f43d4a4c87cba2fc3f6b7571
anjali-sim <anjalighetia123@gmail.com>
2023-04-21 15:53:49 +0530

    :lipstick: Added the css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import 'App.css';
 
 const App = () => {
     return (
         <div>
-            <h1>Hello React</h1>
+            <h1 className='textcolor'>Hello React</h1>
         </div>
     )
 }
