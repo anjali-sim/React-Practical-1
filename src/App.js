990ec07c0fd836ade9d88c3ca49ed6bec0be0c4a
anjali-sim <anjalighetia123@gmail.com>
2023-04-24 10:57:25 +0530

    :lipstick: Updated the style file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import 'App.css';
+import React from "react";
+import "App.css";
 
 const App = () => {
-    return (
-        <div>
-            <h1 className='textcolor'>Hello React</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="textcolor">Hello React</h1>
+      <div id="center">
+        <p>This is my <strong>First React App</strong> from scratch. For this practical, I have created the whole App from scratch, by installing all the packages and webpack. I have installed the css-loader to make the css work and also installed file-loader to include the images in the App.</p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
