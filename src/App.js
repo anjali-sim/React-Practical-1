8b02addf2367b3a36fe32515ee7c61bf604fe253
anjali-sim <anjalighetia123@gmail.com>
2023-04-24 11:12:06 +0530

    :zap: Added the image in the UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "App.css";
+import image from "./image.jpg";
 
 const App = () => {
   return (
@@ -7,6 +8,9 @@ const App = () => {
       <h1 className="textcolor">Hello React</h1>
       <div id="center">
         <p>This is my <strong>First React App</strong> from scratch. For this practical, I have created the whole App from scratch, by installing all the packages and webpack. I have installed the css-loader to make the css work and also installed file-loader to include the images in the App.</p>
+      </div>
+      <div id="centered">
+      <img src={image} alt="Motivational Quote" />
       </div>
     </div>
   );
